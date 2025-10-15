db859f30d25fd6d8086fd9d3a3adec9d12559241
Odette Gallo <odette.gallo@hotmail.com>
2025-10-14 22:56:10 -0300

    boostrap next

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+import {BApp} from 'bootstrap-vue-next'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<BApp>
+  <router-view />
+</BApp>
 </template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
 
